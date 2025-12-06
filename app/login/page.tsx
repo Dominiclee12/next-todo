7667d25946c9587761e2e6d402ed9df8c1a54307
Dominic Lee <dominic_lzl@hotmail.com>
2025-12-06 22:28:00 +0800

    Session store token for todo api services

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,20 +1,48 @@
+"use client";
+
 import Link from "next/link";
-import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
+import { loginAsync } from "../services/api";
 
 export default function LoginPage() {
+	const router = useRouter();
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 
+	const handleSubmit = async (e: FormEvent) => {
+		e.preventDefault();
+
+		const res = await loginAsync(email, password);
+		const token = res.token;
+		console.log("Token: " + token);
+
+		if (token) {
+			sessionStorage.setItem("token", token);
+			router.push("/");
+		}
+	};
+
 	return (
-		<form>
+		<form onSubmit={handleSubmit}>
 			<fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4 mx-auto mt-8">
 				<legend className="fieldset-legend">Login</legend>
 
 				<label className="label">Email</label>
-				<input type="email" className="input" placeholder="Email" />
+				<input
+					type="email"
+					className="input"
+					onChange={(e) => setEmail(e.target.value)}
+					placeholder="Email"
+				/>
 
 				<label className="label">Password</label>
-				<input type="password" className="input" placeholder="Password" />
+				<input
+					type="password"
+					className="input"
+					onChange={(e) => setPassword(e.target.value)}
+					placeholder="Password"
+				/>
 
 				<button className="btn btn-neutral mt-4">Login</button>
 				<p className="mt-2 text-center">
--- app/services/api.ts
+++ app/services/api.ts
@@ -1,16 +1,23 @@
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export async function getTodosAsync() {
-	const res = await fetch(`${API_BASE_URL}/Todo`);
+	const token = sessionStorage.getItem("token");
+	const res = await fetch(`${API_BASE_URL}/Todo`, {
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	});
 
 	return await res.json();
 }
 
 export async function createTodoAsync(title: string) {
+	const token = sessionStorage.getItem("token");
 	const res = await fetch(`${API_BASE_URL}/Todo`, {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
+			Authorization: `Bearer ${token}`,
 		},
 		body: JSON.stringify({ title }),
 	});
@@ -19,10 +26,12 @@ export async function createTodoAsync(title: string) {
 }
 
 export async function toggleCompleteAsync(id: number, completed: boolean) {
+	const token = sessionStorage.getItem("token");
 	const res = await fetch(`${API_BASE_URL}/Todo/${id}`, {
 		method: "PUT",
 		headers: {
 			"Content-Type": "application/json",
+			Authorization: `Bearer ${token}`,
 		},
 		body: JSON.stringify({ completed: !completed }),
 	});
@@ -31,7 +40,23 @@ export async function toggleCompleteAsync(id: number, completed: boolean) {
 }
 
 export async function deleteTodoAsync(id: number) {
+	const token = sessionStorage.getItem("token");
 	const res = await fetch(`${API_BASE_URL}/Todo/${id}`, {
 		method: "DELETE",
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
 	});
+}
+
+export async function loginAsync(email: string, password: string) {
+	const res = await fetch(`${API_BASE_URL}/Auth/login`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ email, password }),
+	});
+
+	return await res.json();
 }
